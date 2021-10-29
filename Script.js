c3972638dc0a65af28edc223561ece3c4db046ef
samod singh <samod0098@gmail.com>
2021-10-29 16:47:50 +0530

    Guass number

---
+++ Script.js
@@ -0,0 +1,10 @@
+let num = Math.ceil(Math.random() * 10);
+console.log(num)
+let gnum = prompt('Guess the number berween 1 and 10 inclus', '');
+if(num == gnum){
+    console.log("Hii You are Great")
+}else{
+    console.log("Try to Again")
+}3
+
+41
